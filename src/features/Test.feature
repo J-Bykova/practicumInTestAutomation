714ca0b760baf9bb060512937327b6a724bb05af
Jenny B <jenia.bykova@gmail.com>
2022-08-22 15:17:08 -0700

    Add homework 6

--- src/features/Test.feature
+++ src/features/Test.feature
@@ -1,13 +1,35 @@
 Feature: Regression test for eBay.com
 
-  Scenario: User could filter results
+  Background:
     Given I navigate to 'ebay'
+
+  @example1
+  Scenario Outline: Example using of Scenario Outline
     When I search for 'scott bike'
-    And For 'Bike Type' I set 'Gravel Bike'
-    And For 'Material' I set 'Carbon Fiber'
-    And For 'Color' I set 'Green'
-    Then I verify page result should contains 'scott bike'
+    And For '<filter>' I set '<value>'
+    Then I verify title of product on page result it should contains '<value>'
+    Examples:
+      | filter    | value        |
+      | Bike Type | Gravel Bike  |
+      | Color     | Green        |
+      | Material  | Carbon Fiber |
 
-
-
+  @example2
+  Scenario: Example using of text block
+    When I click 'Brand Outlet' in top navigation menu
+    Then I print text block
+    """
+    Find it all at
+    the Brand Outlet
+    Your one-stop shop for home,
+    fashion, tech, and more.
+    """
+    When I click 'Help & Contact' in top navigation menu
+    Then I print text block
+    """
+    To keep eBay a safe place to buy and sell,
+    we will occasionally ask you to verify yourself.
+    This helps us to block unauthorized users
+    from entering our site.
+    """
 
--- src/features/steps/definitions.py
+++ src/features/steps/definitions.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 
 @step("I search for '{product}'")
-def search_for_item(context, product):
+def step_impl(context, product):
     search_field = context.driver.find_element(by=By.XPATH, value="//input[@placeholder='Search for anything']")
     search_button = context.driver.find_element(by=By.XPATH, value="//input[@type='submit' and @value='Search']")
 
@@ -12,16 +12,21 @@ def search_for_item(context, product):
 
 
 @step("For '{filter_type}' I set '{filter_value}'")
-def set_filter(context, filter_type, filter_value):
+def step_impl(context, filter_type, filter_value):
     context.driver.find_element(
         by=By.XPATH,
         value=f"//li[.//h3[text()='{filter_type}']]//span[text()='{filter_value}']").click()
 
 
-@step("I verify page result should contains '{expected}'")
-def verify_result(context, expected):
+@step("I verify title of product on page result it should contains '{expected}'")
+def step_impl(context, expected):
     actual_title = context.driver.find_element(
         by=By.XPATH,
         value="(//*[contains(@class,'srp-results')]//h3[@class='s-item__title'])[1]"
     )
-    assert expected in actual_title.text, "There is a bug"
+    assert expected in actual_title.text, f"There is a bug: Title does not contains {expected}"
+
+
+@step("I print text block")
+def step_impl(context):
+    print(f"I print text block: \n{context.text}\n")
