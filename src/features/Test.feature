de1584c2a61b0489a18387c84cd90bd2882f1ce9
Jenny B <jenia.bykova@gmail.com>
2022-08-22 22:10:19 -0700

    Working on project

--- src/features/Test.feature
+++ src/features/Test.feature
@@ -1,10 +1,34 @@
+@example
 Feature: Regression test for eBay.com
 
   Background:
     Given I navigate to 'ebay'
 
-  @example1
-  Scenario Outline: Example using of Scenario Outline
+
+  @example5
+  Scenario: Verify information about Captcha
+    When I click 'Sign in' in top navigation menu
+    And I click on 'I am human' checkbox
+    And I open menu of captcha and click on 'information' button
+    Then I validate the 'information' popup
+    """
+    hCaptcha is a service that reduces bots and spam by asking simple questions.
+    Please follow the instructions at the top of the screen for each challenge.
+    For more information visit
+    """
+
+  @example4
+  Scenario: Verify the product’s multiple filters on the result page
+    When I search for 'scott bike'
+    And I set a few filters
+      | filter    | value       |
+      | Bike Type | Gravel Bike |
+      | Condition | Used        |
+    Then I validate that all results are related to 'Gravel Bike' and 'Pre-Owned'
+
+
+  @example3
+  Scenario Outline:  Verify the product’s one filter on the result page
     When I search for 'scott bike'
     And For '<filter>' I set '<value>'
     Then I verify title of product on page result it should contains '<value>'
@@ -14,22 +38,21 @@ Feature: Regression test for eBay.com
       | Color     | Green        |
       | Material  | Carbon Fiber |
 
-  @example2
-  Scenario: Example using of text block
-    When I click 'Brand Outlet' in top navigation menu
-    Then I print text block
-    """
-    Find it all at
-    the Brand Outlet
-    Your one-stop shop for home,
-    fashion, tech, and more.
-    """
-    When I click 'Help & Contact' in top navigation menu
-    Then I print text block
-    """
-    To keep eBay a safe place to buy and sell,
-    we will occasionally ask you to verify yourself.
-    This helps us to block unauthorized users
-    from entering our site.
-    """
 
+  @example2
+  Scenario Outline: Verify sub-category navigation leads to right pages
+    When I click 'Shop by category' and pick '<category>'
+    Then  I expected to see '<title>' of page
+    Examples:
+      | category           | title                                        |
+      | Collectibles & Art | Collectibles & Art products for sale \| eBay |
+      | Home & garden      | Home & Garden products for sale \| eBay      |
+
+  @example1
+  Scenario Outline: Verify top navigation menu items leads to right pages
+    When I click '<link>' in top navigation menu
+    Then  I expected to see '<title>' of page
+    Examples:
+      | link         | title                                               |
+      | Daily Deals  | Daily Deals on eBay \| Best deals and Free Shipping |
+      | Brand Outlet | Brand Outlet products for sale \| eBay              |
--- src/features/steps/definitions.py
+++ src/features/steps/definitions.py
@@ -1,21 +1,14 @@
+import time
+
 from behave import step
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.expected_conditions import title_contains, title_is
+from selenium.webdriver.support.wait import WebDriverWait
 
 
-@step("I search for '{product}'")
-def step_impl(context, product):
-    search_field = context.driver.find_element(by=By.XPATH, value="//input[@placeholder='Search for anything']")
-    search_button = context.driver.find_element(by=By.XPATH, value="//input[@type='submit' and @value='Search']")
-
-    search_field.send_keys(product)
-    search_button.click()
-
-
-@step("For '{filter_type}' I set '{filter_value}'")
-def step_impl(context, filter_type, filter_value):
-    context.driver.find_element(
-        by=By.XPATH,
-        value=f"//li[.//h3[text()='{filter_type}']]//span[text()='{filter_value}']").click()
+@step("I validate that all results are related to '{filter_one}' and '{filter_two}'")
+def step_impl(context, filter_one, filter_two):
 
 
 @step("I verify title of product on page result it should contains '{expected}'")
@@ -27,6 +20,76 @@ def step_impl(context, expected):
     assert expected in actual_title.text, f"There is a bug: Title does not contains {expected}"
 
 
+@step("For '{filter_type}' I set '{filter_value}'")
+def step_impl(context, filter_type, filter_value):
+    context.driver.find_element(
+        by=By.XPATH,
+        value=f"//li[.//h3[text()='{filter_type}']]//span[text()='{filter_value}']").click()
+
+
+@step("I search for '{product}'")
+def step_impl(context, product):
+    search_field = context.driver.find_element(by=By.XPATH, value="//input[@placeholder='Search for anything']")
+    search_button = context.driver.find_element(by=By.XPATH, value="//input[@type='submit' and @value='Search']")
+
+    search_field.send_keys(product)
+    search_button.click()
+
+
+@step("I set a few filters")
+def step_impl(context):
+    table_filters = context.table
+
+    for row in table_filters:
+        filter_type = row['filter']
+        filter_value = row['value']
+
+        filter_option = context.driver.find_element(
+            by=By.XPATH,
+            value=f"//li[.//h3[text()='{filter_type}']]//span[text()='{filter_value}']")
+        filter_option.click()
+
+
 @step("I print text block")
 def step_impl(context):
     print(f"I print text block: \n{context.text}\n")
+
+
+@step("I expected to see '{expected}' of page")
+def step_impl(context, expected):
+    WebDriverWait(context.driver, timeout=3).until(title_is(expected))
+
+
+@step("I click '{shop_by}' and pick '{category}'")
+def step_impl(context, shop_by, category):
+    menu_button = context.driver.find_element(
+        by=By.XPATH,
+        value=f"//button[contains(text(), '{shop_by}')]"
+    )
+    menu_button.click()
+
+    category_item = context.driver.find_element(
+        by=By.XPATH,
+        value=f"//div[./h2[text()='{shop_by}']]//a[text()='{category}']"
+    )
+    category_item.click()
+
+
+@step("I click '{link}' in top navigation menu")
+def step_impl(context, link):
+    item = context.driver.find_element(
+        by=By.XPATH,
+        value=f"//header//a[contains(text(), '{link}')]"
+    )
+    item.click()
+
+
+@step("I navigate to '{page}'")
+def step_impl(context, page):
+    context.driver = webdriver.Chrome(executable_path="/Users/jenny/bin/webdriver/chromedriver")
+    if page == "google":
+        return context.driver.get("https://www.google.com/")
+    elif page == "ebay":
+        return context.driver.get("https://www.ebay.com/")
+    else:
+        print(f"'{page}' is unexpected page")
